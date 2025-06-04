2d36693d25bf3246bdda5a1befeb13774dd74608
fuhadsaneenv <saneenfuhad81@gmail.com>
2025-06-04 12:12:24 +0530

    Updated Agent side

--- backend/routes/agentRoutes.js
+++ backend/routes/agentRoutes.js
@@ -1,14 +1,14 @@
-
 import express from 'express';
-import { createProperty,getMyProperties,editProperty,deleteProperty } from '../controllers/agentController.js';
+import { createProperty, getMyProperties, editProperty, deleteProperty, getAgentStats } from '../controllers/agentController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import upload from '../middleware/uploadMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', protect, upload.array('images', 5), createProperty); 
-router.get('/my', protect, getMyProperties); 
-router.put('/:id', protect, upload.array('images', 5), editProperty); 
-router.delete('/:id', protect, deleteProperty); 
+router.post('/', protect, upload.array('images', 5), createProperty);
+router.get('/my', protect, getMyProperties);
+router.put('/:propertyId', protect, upload.array('images', 5), editProperty);
+router.delete('/:propertyId', protect, deleteProperty); 
+router.get('/stats', protect, getAgentStats);
 
 export default router;
