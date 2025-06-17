2aaed8915ab10d8f629e5e1699897536b6c63bc4
fuhadsaneenv <saneenfuhad81@gmail.com>
2025-06-17 10:54:54 +0530

    adminside

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
@@ -26,6 +27,34 @@ import About from "./pages/About";
 import { ProtectedRoute } from "./ProtectedRoutes/ProtectedRoute";
 import UserDetails from "./pages/admin/userDetails";
 import AgentDetails from "./pages/admin/AgentDetails";
+=======
+import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Profile from './pages/user/Profile'
+import Footer from './components/Footer'
+import PropertyListing from './pages/PropertListing'
+import PropertyDetail from './pages/PropertDetail'
+import AgentLayout from './layouts/AgentLayout'
+import AgentDashboard from './pages/Agent/AgentDashboard'
+import AgentProperties from './pages/Agent/AgentProperties'
+import AgentMessages from './pages/Agent/AgentMessages'
+import AgentEnquiries from './pages/Agent/AgentEnquiries'
+import { AdminSidebar } from './pages/admin/AdminSidebar'
+import AdminLayout from './layouts/AdminLayout'
+import AdminDashboard from './pages/admin/AdminDashboard'
+import AdminUserManagement from './pages/admin/AdminuserManagement'
+import AdminProperty from './pages/admin/AdminProperty'
+import AdminSubscription from './pages/admin/AdminSubctription'
+import AdminBooking from './pages/admin/AdminBooking'
+import { AuthStateChecker } from './components/AuthStateChecker'
+import RoleRouter from './ProtectedRoutes/RoleRouter'
+import AgentListingPage from './pages/AgentListing'
+import AgentDetailsPage from './pages/AgentDetails'
+import About from './pages/About'
+import { ProtectedRoute } from './ProtectedRoutes/ProtectedRoute'
+>>>>>>> upstream/dev
 
 const App = () => {
   const location = useLocation();
